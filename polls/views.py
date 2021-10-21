4433d5d6cddede0907407a310e3e2496ccb40a9b
BriscanCatalin <c_briscan@yahoo.com>
2021-10-21 10:20:24 +0300

    Poll works fine.

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.views import generic
 from .models import Question, Choice
+from django.urls import reverse
 # Create your views here.
 
 
