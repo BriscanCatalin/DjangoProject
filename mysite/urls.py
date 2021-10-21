417d6566043ca21da733496e389a6a2283235d36
BriscanCatalin <c_briscan@yahoo.com>
2021-10-21 12:26:55 +0300

    Home page is renderes fine !

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
-
+from mysite import views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
